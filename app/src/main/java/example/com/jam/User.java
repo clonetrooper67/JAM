7804bbd70bdfe6ac8c6fdf11e0f9dca1e2f0ac6e
sdoulen <sdoulen@gmail.com>
2016-10-22 18:44:56 -0400

    user Class file

--- app/src/main/java/example/com/jam/User.java
+++ app/src/main/java/example/com/jam/User.java
@@ -1,10 +1,16 @@
 package example.com.jam;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Shane on 10/22/2016.
  */
 
 public class User {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    Date date;
     private int rating = 0;
     private String uName = " ";
     private String contact = " ";
@@ -16,17 +22,23 @@ public class User {
 
     public User(String n) {
         uName = n;
+        contact = " ";
+        desc = " ";
+        date = new Date();
     }
 
     public User(String n, String c) {
         uName = n;
         contact = c;
+        desc = " ";
+        date = new Date();
     }
 
     public User(String n, String c, String d) {
         uName = n;
         contact = c;
         desc = d;
+        date = new Date();
     }
 
     public void setRating(int r) {
@@ -60,4 +72,6 @@ public class User {
     public String getDesc() {
         return desc;
     }
+
+    public Date getDate() { return date; }
 }
