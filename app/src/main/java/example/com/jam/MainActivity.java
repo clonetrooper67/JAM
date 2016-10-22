8199ab17d9dffff10eb47a420227b2295d5b6ba2
Joshua Hutchins <ninjosh97@gmail.com>
2016-10-22 15:51:58 -0400

    Added a comment to MainActivity

--- app/src/main/java/example/com/jam/MainActivity.java
+++ app/src/main/java/example/com/jam/MainActivity.java
@@ -12,7 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
+// Hello World! --Josh
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
