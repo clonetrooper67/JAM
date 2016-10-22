3e2b88a360dc728ac5907f47c32f22eea190f354
Waptop <number45zephan@gmail.com>
2016-10-22 15:53:09 -0400

    Test Commit for android studio

--- app/src/main/java/example/com/jam/MainActivity.java
+++ app/src/main/java/example/com/jam/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 // Hello World! --Josh
+// Hello World! --Zephan
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
