c3cd55131d894795a84ac38ff0c74fa265b26d04
Waptop <number45zephan@gmail.com>
2016-10-22 15:55:11 -0400

    Added my message!

--- app/src/main/java/example/com/jam/MainActivity.java
+++ app/src/main/java/example/com/jam/MainActivity.java
@@ -13,7 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 // Hello World! --Josh
-// Hello World! --Zephan
+// Hello World! --Zephanoh
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
