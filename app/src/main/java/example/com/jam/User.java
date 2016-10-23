a18b186dd1c8d7122a7d8cae768f48dc1193868d
cdyson128 <cdyson@gmail.com>
2016-10-23 02:05:43 -0400

    User class update

--- app/src/main/java/example/com/jam/User.java
+++ app/src/main/java/example/com/jam/User.java
@@ -1,8 +1,10 @@
 package example.com.jam;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by Shane on 10/22/2016.
@@ -11,50 +13,63 @@ import java.util.Date;
 public class User {
     DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     Date date;
-    private int rating = 0;
-    private String uName = " ";
-    private String contact = " ";
-    private String desc = " ";
-    // image file?
+    private int mKarma = 0;
+    private String fName;
+    private String lName;
+    private String contact;
+    private String desc;
+    private UUID mId;
+    private File mProfileImage;
 
     public User() {
     }
 
-    public User(String n) {
-        uName = n;
+    public User(String n, String l) {
+        fName = n;
+        lName = l;
         contact = " ";
         desc = " ";
         date = new Date();
     }
 
-    public User(String n, String c) {
-        uName = n;
+    public User(String n, String l,  String c) {
+        fName = n;
+        lName = l;
         contact = c;
         desc = " ";
         date = new Date();
     }
 
-    public User(String n, String c, String d) {
-        uName = n;
+    public User(String n, String l ,String c, String d) {
+        fName = n;
+        lName = l;
         contact = c;
         desc = d;
         date = new Date();
     }
 
-    public void setRating(int r) {
-        rating = r;
+    public void setKarma(int r) {
+        mKarma = r;
     }
 
-    public int getRating() {
-        return rating;
+    public int getKarma() {
+        return mKarma;
     }
 
-    public void setuName(String n) {
-        uName = n;
+    public void setfName(String n) {
+        fName = n;
     }
 
-    public String getuName() {
-        return uName;
+    public void setlName(String l) {
+        lName = l;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
     }
 
     public void setContact(String c) {
@@ -74,4 +89,20 @@ public class User {
     }
 
     public Date getDate() { return date; }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public File getProfileImage() {
+        return mProfileImage;
+    }
+
+    public void setProfileImage(File mProfileImage) {
+        this.mProfileImage = mProfileImage;
+    }
 }
