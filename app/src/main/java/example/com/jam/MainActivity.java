31e5f3093df47ed3b8fbcef98635e1e8f77fca8b
Waptop <number45zephan@gmail.com>
2016-10-22 20:46:43 -0400

    Added the customtab libraries and changed the links on the drawer to link to the website

--- app/src/main/java/example/com/jam/MainActivity.java
+++ app/src/main/java/example/com/jam/MainActivity.java
@@ -1,11 +1,12 @@
 package example.com.jam;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 // Hello World! --Josh
 // Hello World! --Zephanoh
 // Hello World! --Shane
@@ -91,18 +93,24 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.program_request) {
+            openPage("https://triadja.org/programs/junior-achievement-program-request/");
+        }
+        else if (id == R.id.volunteer_registration) {
+            openPage("https://triadja.org/classroom-volunteers/volunteer-registration/");
+        } else if (id == R.id.website_link) {
+            openPage("https://triadja.org/");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public void openPage(String url){
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(url));
     }
 }
